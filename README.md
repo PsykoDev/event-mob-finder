b6d016fb639ca6cf49101e8e18824fa687e364fd
valkyr1e <nkpoidjp+tera@gmail.com>
2018-12-04 02:51:15 +0900

    support auto update

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# Event-Mob-Finder
+Event-Mob-Finder
+===
 Tera proxy module
 
 Marks every event npc
-
-requires command module
-
-to toggle type in chat: /8 eventmob
--- index.js
+++ index.js
@@ -1,58 +1,31 @@
-//written by Bubble
-const Command = require('command');
+module.exports = function EventMobFinder(mod) {
+  let npcs = []
 
-module.exports = function EventMobFinder(dispatch) {
-	const command = Command(dispatch);
-	let enabled = true;
-	let npcs = [];
-	
-	command.add('eventmob', () => {
-		enabled = !enabled;
-		command.message('Event mob finder '+(enabled?'enabled':'disabled')+'.');
-	});
-	
-	dispatch.hook('S_SPAWN_NPC', 8, event => {
-		if((event.unk20&0xFF) === 1) {
-			npcs[Number(event.gameId)] = event;
-			var source;
-			if (dispatch.base.proxyAuthor == "caali") {
-				event.gameId = {low:event.gameId.low, high:0, unsigned:true};
-				source = 0;
-			} else {
-				event.gameId = event.gameId & BigInt(0xFFFF);
-				source = BigInt(0);
-			}
-			dispatch.toClient('S_SPAWN_DROPITEM', 6, {
-				gameId: event.gameId,
-				loc: event.loc,
-				item: 98260,
-				amount: 1,
-				expiry: 30000,
-				explode: false,
-				masterwork: false,
-				enchant: 0,
-				source: source,
-				debug: false,
-				owners: [{id: 0}]
-			});
-			notice();
-			command.message('Found event mob.');
-		}
-	});
-	
-	dispatch.hook('S_DESPAWN_NPC', 3, event => {
-		if(npcs[Number(event.gameId)]) {
-			dispatch.toClient('S_DESPAWN_DROPITEM', 1, {id:npcs[event.gameId].gameId} );
-			delete npcs[event.gameId];
-		}
-	});
-	
-	function notice() {
-		dispatch.toClient('S_DUNGEON_EVENT_MESSAGE', 1, {
-            unk1: 2,
-            unk2: 0,
-            unk3: 0,
-            message: 'Found event mob.'
-        });
+  mod.hook('S_SPAWN_NPC', 10, event => {
+    if (event.bySpawnEvent) {
+      const id = event.gameId.low
+      npcs.push(id)
+      mod.send('S_SPAWN_DROPITEM', 6, {
+        gameId: { low: id, high: 0, unsigned: true },
+        loc: event.loc,
+        item: 98260,
+        amount: 1
+      })
+      mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
+        type: 2,
+        chat: false,
+        channel: 0,
+        message: 'Found event mob.'
+      })
     }
-};
+  })
+
+  mod.hook('S_DESPAWN_NPC', 3, event => {
+    const id = event.gameId.low
+    mod.send('S_DESPAWN_DROPITEM', 4, {
+      gameId: { low: id, high: 0, unsigned: true }
+    })
+    if (npcs.includes(id))
+      npcs = npcs.filter(npc => npc !== id)
+  })
+}
