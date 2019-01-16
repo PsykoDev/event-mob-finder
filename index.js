75e6ce24635725634584143d4c835036445e6527
valkyr1e <nkpoidjp+tera@gmail.com>
2019-01-16 19:18:58 +0900

    update protocol

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 module.exports = function EventMobFinder(mod) {
   let npcs = []
 
-  mod.hook('S_SPAWN_NPC', 10, event => {
+  mod.hook('S_SPAWN_NPC', 11, event => {
     if (event.bySpawnEvent) {
-      const id = Number(event.gameId)
+      const id = event.gameId * 2n
       npcs.push(id)
       mod.send('S_SPAWN_DROPITEM', 6, {
-        gameId: { low: id, high: 0, unsigned: true },
+        gameId: id,
         loc: event.loc,
         item: 98260,
         amount: 1
       })
-      mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
-        type: 2,
-        chat: false,
-        channel: 0,
-        message: 'Found event mob.'
-      })
+      mod.send('S_DUNGEON_EVENT_MESSAGE', 2, { type: 2, message: 'Found event mob.' })
     }
   })
 
   mod.hook('S_DESPAWN_NPC', 3, event => {
-    const id = Number(event.gameId)
+    const id = event.gameId * 2n
     if (npcs.includes(id)) {
-      mod.send('S_DESPAWN_DROPITEM', 4, {
-        gameId: { low: id, high: 0, unsigned: true }
-      })
+      mod.send('S_DESPAWN_DROPITEM', 4, { gameId: id })
       npcs = npcs.filter(npc => npc !== id)
     }
   })
